a3c594c21a8c4e9a8d54e588e858081877ceb0a5
Mohammad1 Yusuf <mohammad1.yusuf@one97.delhi.net>
2024-03-21 01:06:57 +0530

    profile page updated

--- 03.1_Ecom_Dashborad/src/Componants/Profile/Profile.jsx
+++ 03.1_Ecom_Dashborad/src/Componants/Profile/Profile.jsx
@@ -4,6 +4,7 @@ function Profile() {
   return (
     <div className=" flex justify-center h-screen">
       <h1>This is mohammad Yusuf </h1>
+      <h1>Learn react with live projects</h1>
     </div>
   );
 }

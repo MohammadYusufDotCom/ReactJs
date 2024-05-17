221b85ce51cadb4fc3a2129904cab7992e4611b2
Mohammad Yusuf <mohd.yusuf2207@gmail.com>
2024-05-17 23:43:53 +0530

    toggle button is created

---
+++ 07_Contax_API_And_Redux/03_Project_context_API/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import ThemeBtn from './Component/Themebtn'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <ThemeBtn/>
+      
+    </>
+  )
+}
+
+export default App

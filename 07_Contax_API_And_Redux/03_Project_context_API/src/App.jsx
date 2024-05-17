d2f5b1e85444702b15607edb06ef7d76a7ec3f4c
Mohammad Yusuf <mohd.yusuf2207@gmail.com>
2024-05-18 02:12:12 +0530

    card is created

--- 07_Contax_API_And_Redux/03_Project_context_API/src/App.jsx
+++ 07_Contax_API_And_Redux/03_Project_context_API/src/App.jsx
@@ -1,14 +1,23 @@
 import { useState } from 'react'
 import ThemeBtn from './Component/Themebtn'
+import Card from './Component/card'
+import "./App.css"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-     <ThemeBtn/>
-      
-    </>
+    <div className="flex flex-wrap min-h-screen items-center">
+      <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+              <ThemeBtn/>
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+              <Card/>
+          </div>
+      </div>
+    </div>
   )
 }
 

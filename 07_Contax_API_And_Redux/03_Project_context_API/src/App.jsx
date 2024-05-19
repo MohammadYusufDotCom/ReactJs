5b13b4ffd18da4d005e1ed5b63ac3148b23f5787
Mohammad Yusuf <mohd.yusuf2207@gmail.com>
2024-05-20 00:25:59 +0530

    update functionality of card make dynamic card

--- 07_Contax_API_And_Redux/03_Project_context_API/src/App.jsx
+++ 07_Contax_API_And_Redux/03_Project_context_API/src/App.jsx
@@ -4,8 +4,11 @@ import Card from './Component/card'
 import { ThemeProvider } from './Context/Context'
 
 function App() {
-  const [theme, setTheme] = useState("light")
-  const [bodyColor, setBodyColor] = useState("#FFFFFF")
+  const [theme, setTheme] = useState("dark")
+  const [bodyColor, setBodyColor] = useState("#242424")
+  const title1 = "Beautifull flower, White Background, Create Yusuf"
+  const title2 = "People Gathring, Sun Set, Created by Yusuf"
+  const title3 = "Restaurant Place for Peace, White Background, Created By Yusuf"
 
   function darktheme(){
     setTheme("dark")
@@ -13,7 +16,7 @@ function App() {
   }
   function lightTheme(){
     setTheme("light")
-    setBodyColor("#FFFFFF")
+    setBodyColor("#faf9f9")
   }
   useEffect(()=>{
     document.querySelector('html').classList.remove("dark","light")
@@ -22,13 +25,15 @@ function App() {
     },[theme])
   return (
     <ThemeProvider value={{theme,darktheme,lightTheme}}>
-    <div className="flex flex-wrap min-h-screen items-center">
-      <div className="w-full">
-          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+          <div className="w-full p-16  flex justify-end mb-4">
               <ThemeBtn/>
           </div>
-          <div className="w-full max-w-sm mx-auto">
-              <Card/>
+    <div className="flex flex-wrap min-h-screen ">
+      <div className="w-full">
+          <div className="w-full flex  gap-x-4 justify-between max-w-5xl mx-auto">
+              <Card link={'https://tinyurl.com/2p958m3j'} title={title1} price={359}/>
+              <Card link={'https://rb.gy/widlsq'} title={title2} price={499}/>
+              <Card link={'https://tinyurl.com/ubb5h8cb'} title={title3} price={599}/>
           </div>
       </div>
     </div>

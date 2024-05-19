6f41dc0d81e2bd220a1325a2497f3c9fa3ec4fbf
Mohammad Yusuf <mohd.yusuf2207@gmail.com>
2024-05-19 20:59:20 +0530

    toggle theme project ready

--- 07_Contax_API_And_Redux/03_Project_context_API/src/App.jsx
+++ 07_Contax_API_And_Redux/03_Project_context_API/src/App.jsx
@@ -1,23 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import ThemeBtn from './Component/Themebtn'
 import Card from './Component/card'
-import "./App.css"
+import { ThemeProvider } from './Context/Context'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [theme, setTheme] = useState("Light")
+
+  function darktheme(){
+    setTheme("dark")
+  }
+  function lightTheme(){
+    setTheme("light")
+  }
+  useEffect(()=>{
+    document.querySelector('html').classList.remove('light','dark')
+    document.querySelector('html').classList.add(theme)
+    },[theme])
+
+
 
   return (
+    <ThemeProvider value={{theme,darktheme,lightTheme}}>
     <div className="flex flex-wrap min-h-screen items-center">
       <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
               <ThemeBtn/>
           </div>
-
           <div className="w-full max-w-sm mx-auto">
               <Card/>
           </div>
       </div>
     </div>
+    </ThemeProvider>
   )
 }
 
--- 07_Contax_API_And_Redux/03_Project_context_API/src/Component/Themebtn.jsx
+++ 07_Contax_API_And_Redux/03_Project_context_API/src/Component/Themebtn.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { UseTheme } from '../Context/Context';
+
 
 export default function ThemeBtn() {
-    
+
+    const { theme, lightTheme, darktheme } = UseTheme();
+
+    const themeChange=(e)=>{
+        const isChecked = e.target.checked;
+        if (isChecked) darktheme();
+        else lightTheme();
+    }
 
     return (
         <label className="relative inline-flex items-center cursor-pointer ">
@@ -9,6 +18,7 @@ export default function ThemeBtn() {
                 type="checkbox"
                 value=""
                 className="sr-only peer"
+                onChange={themeChange}
             />
             <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none dark:peer-focus:ring-blue-800 rounded-full dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
 

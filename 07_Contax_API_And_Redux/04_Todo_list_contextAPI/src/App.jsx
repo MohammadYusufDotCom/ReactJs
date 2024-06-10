44963bc529b2e3f1f92e730483b591b64f4825ea
Mohammad Yusuf <mohd.yusuf2207@gmail.com>
2024-06-11 00:54:58 +0530

    update basic funcnality

--- 07_Contax_API_And_Redux/04_Todo_list_contextAPI/src/App.jsx
+++ 07_Contax_API_And_Redux/04_Todo_list_contextAPI/src/App.jsx
@@ -3,9 +3,20 @@ import { useTodo, todoProvider } from "./Context";
 
 
 function App() {
-  const [todo,setTodo] = useState([])
+  const [todos,setTodos] = useState([])
+
+  const addTodo =(todo)=>{
+    setTodos((prev)=>[{id: Date.now(),...todo},...prev])
+  }
+  const updateTodo=(id,todo)=>{
+    setTodos((prev)=>prev.map((prevTodo)=>prevTodo.id === id ? todo : prevTodo))
+  }
+  const deleteTodo=(id)=>{
+    setTodos((preTodo)=>preTodo.filter((prev)=> prev.id !== id))
+  }
+  
   return (
-    <todoProvider value={{todos,addaddTodo,deleteTodo,updateTodo,completeTodo}}>
+    <todoProvider value={{todos,addTodo,deleteTodo,updateTodo,completeTodo}}>
     <div className="bg-[#172842] min-h-screen py-8">
       <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
         <h1 className="text-2xl font-bold text-center mb-8 mt-2">
